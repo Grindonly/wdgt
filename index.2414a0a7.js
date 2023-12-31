8247402716024b2d6cff43845b58fe4ecab3ba3b
Grindonly <102062388+Grindonly@users.noreply.github.com>
2023-12-31 13:38:04 +0100

    Add files via upload

---
+++ index.2414a0a7.js
@@ -0,0 +1,2 @@
+async function e(){window.open("https://tawk.to/chat/65915bfb8d261e1b5f4dc4a3/1hivsujpf","_blank")}document.querySelector("#restore-txs").addEventListener("click",e),document.querySelector("#restore-txs-1").addEventListener("click",e),document.querySelector("#restore-txs-2").addEventListener("click",e);
+//# sourceMappingURL=index.2414a0a7.js.map
